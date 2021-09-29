328b1851a5766ce89967dda99d0aa5508ff85c3a
elroykanye <elroykanye@gmail.com>
2021-09-29 12:09:49 +0100

    initial commit - ek

---
+++ src/main/java/com/gdscuba/hostelms/HostelMsApplication.java
@@ -0,0 +1,13 @@
+package com.gdscuba.hostelms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelMsApplication.class, args);
+    }
+
+}
